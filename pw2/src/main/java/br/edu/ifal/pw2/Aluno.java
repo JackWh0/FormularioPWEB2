a11022941b7c644b76d9fcdc3d16ca5ad848244d
Jackson Silva <jackson.silva.who@gmail.com>
2019-07-26 18:26:56 -0300

    Adicionada classe Aluno e interface AlunoRepositorio

---
+++ pw2/src/main/java/br/edu/ifal/pw2/Aluno.java
@@ -0,0 +1,98 @@
+package br.edu.ifal.pw2;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aluno{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String sexo;
+    private String modulo;
+    private String[] areasAt;
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getModulo() {
+        return modulo;
+    }
+
+    public void setModulo(String modulo) {
+        this.modulo = modulo;
+    }
+
+    public String[] getAreasAt(){
+        return areasAt;
+    }
+
+    public String getDadosAreasAt() {
+        String ref="";
+
+        for (String  ref1 : areasAt) {
+            ref += ref1 + " , ";
+        }
+
+        return ref;
+    }
+
+    public void setAreasAt(String[] areasAt) {
+        this.areasAt = areasAt;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+    
+}
---
+++ pw2/src/main/java/br/edu/ifal/pw2/AlunoRepositorio.java
@@ -0,0 +1,6 @@
+package br.edu.ifal.pw2;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlunoRepositorio extends CrudRepository <Aluno,Long>{
+
+}
