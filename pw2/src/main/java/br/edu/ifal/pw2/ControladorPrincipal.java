890961877cbd20e7d8bfb0bb35d17dbb74c52e37
Anderson <adnrepper@gmail.com>
2019-08-08 23:07:18 -0300

    Metodos de atualizar, deletar e listar adicionados a classe ControladorPrincipal. Imagens também Adicionadas

--- pw2/src/main/java/br/edu/ifal/pw2/ControladorPrincipal.java
+++ pw2/src/main/java/br/edu/ifal/pw2/ControladorPrincipal.java
@@ -1,10 +1,13 @@
 package br.edu.ifal.pw2;
 
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 public class ControladorPrincipal{
@@ -17,26 +20,49 @@ public class ControladorPrincipal{
         return new ModelAndView("form.html");
     }
     @RequestMapping("/cadastro")
-    public String cadastro(Aluno aluno){
-        String infoCadastrados = "Alunos cadastrados: <br>";
+    public ModelAndView cadastroAluno(Aluno aluno, RedirectAttributes redirect) {
         rep.save(aluno);
-
-        Iterable<Aluno> alunos = rep.findAll();    
-       // ModelAndView resposta = new ModelAndView("form.html");
-        for (Aluno al : alunos) {
-
-        //resposta.addObject("nome", al.getNome());
-        //resposta.addObject("email", al.getEmail());
-        //resposta.addObject("cpf", al.getCpf());
-        //resposta.addObject("sexo", al.getSexo());
-        //resposta.addObject("modulo", al.getModulo());
-        //resposta.addObject("areasAt", al.getDadosAreasAt());
-        //resposta.addObject("senha", al.getSenha());
-        infoCadastrados += "[Nome: " + al.getNome() + ", Email: " + al.getEmail() + ", CPF: " +
-        al.getCpf() + ", Sexo: " + al.getSexo() + ", Módulo: " + al.getModulo()
-         + ", Senha: " + al.getSenha() + "]" + "<br>";
-            
-        }
-        return infoCadastrados + "<br> <a href='/'>Clique para voltar para o formulário</a>";
+        ModelAndView retorno = new ModelAndView("redirect:/listar_alunos");
+        redirect.addFlashAttribute("mensagem", aluno.getNome() + " cadastrado com sucesso.");
+        return retorno;
     }
+   
+
+    @RequestMapping("/listar_alunos")
+    public ModelAndView listarAlunos() {
+        ModelAndView retorno = new ModelAndView("listar_alunos.html");
+        Iterable<Aluno> alunos = rep.findAll();
+        retorno.addObject("alunos", alunos);
+        return retorno;
+    }
+
+    @RequestMapping("/excluir_aluno/{idAluno}")
+    public ModelAndView excluirAlunos(@PathVariable("idAluno") Long alunoID, RedirectAttributes redirect) {
+        Optional<Aluno> opcao = rep.findById(alunoID);
+        ModelAndView retorno = new ModelAndView("redirect:/listar_alunos");        
+        if(opcao.isPresent()){
+            Aluno a = opcao.get();
+            rep.deleteById(a.getId());            
+            redirect.addFlashAttribute("mensagem", a.getNome() + " excluído com sucesso.");
+            return retorno;
+        } else {
+            redirect.addFlashAttribute("mensagem", "Aluno " + alunoID + " não existe no Banco de Dados.");
+            return retorno;
+        }
+        
+    }
+
+    @RequestMapping("/atualizar_aluno/{idAluno}")
+    public ModelAndView atualizar(@PathVariable("idAluno") Long alunoID) {
+        Optional<Aluno> opcao = rep.findById(alunoID);
+        ModelAndView retorno = new ModelAndView("formulario.html");
+        if(opcao.isPresent()){
+            Aluno a = opcao.get();
+            retorno.addObject("aluno", a);
+            return retorno;
+        }
+        return retorno;
+    
+
+}
 }
